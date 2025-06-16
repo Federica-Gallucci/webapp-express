579e635455d7ef10ca18473de03f65faaf1783b6
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-06-16 18:08:01 +0200

    modify index and show

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,26 +1,26 @@
 const connection = require("../db/conn.js");
 
 const index = (req, res) => {
-  const sql = `SELECT * FROM posts`;
+  const sql = `SELECT * FROM movies`;
   connection.query(sql, (err, results) => {
-    if (err) throw err;
-    console.log(results);
-    // if (err)
-    //   return res.status(500).json({
-    //     error: `Database query failed`,
-    //   });
-    // res.json(results);
+    // if (err) throw err;
+    // console.log(results);
+    if (err)
+      return res.status(500).json({
+        err: `Database query failed`,
+      });
+    res.json(results);
   });
 };
 
 const show = (req, res) => {
   const id = req.params.id;
-  const sql = `SELECT * FROM posts WHERE id =?`;
-  connection.query(sql, [id], (error, results) => {
-    if (err) return res.status(500).json({ error: `Database query failed` });
+  const sql = `SELECT * FROM movies WHERE id =?`;
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ err: `Database query failed` });
     if (results.length === 0)
-      return res.status(404).json({ error: `Movie not found` });
-    res.json(result[0]);
+      return res.status(404).json({ err: `Movie not found` });
+    res.json(results[0]);
   });
 };
 
